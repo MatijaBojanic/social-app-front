bfb5f51c1e4bc9d560eb4a3fec6712a692bce519
MatijaBojanic <matija.bojanic@gmail.com>
2024-06-01 13:51:51 +0200

    App div now takes the whole screen space

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,20 @@ export default {
 
 
 <style>
+html, body, #app {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden; /* Prevents scrollbars if not necessary */
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
+
